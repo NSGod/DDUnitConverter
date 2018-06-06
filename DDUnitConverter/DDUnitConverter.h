90bfc8bdd1eac6ed04709a9af25130e0df8392af
Udit Agarwal <udita@amazon.com>
2018-06-06 23:51:07 +0530

    Revert "Add all converter headers in unbrella header"
    This reverts commit 6e61ae44cc6a8faec17ac9d71c64fdfa165ba40e.

--- DDUnitConverter/DDUnitConverter.h
+++ DDUnitConverter/DDUnitConverter.h
@@ -9,33 +9,6 @@
 #import <Foundation/Foundation.h>
 #import "DDUnitDetails.h"
 
-//All converters
-
-#import "DDAccelerationUnitConverter.h"
-#import "DDAngleUnitConverter.h"
-#import "DDAreaUnitConverter.h"
-#import "DDByteUnitConverter.h"
-#import "DDCurrentUnitConverter.h"
-#import "DDCurrentUnitConverter.h"
-#import "DDEnergyUnitConverter.h"
-#import "DDForceUnitConverter.h"
-#import "DDIlluminationUnitConverter.h"
-#import "DDInductanceUnitConverter.h"
-#import "DDLengthUnitConverter.h"
-#import "DDMagnitudeUnitConverter.h"
-#import "DDMassUnitConverter.h"
-#import "DDPowerUnitConverter.h"
-#import "DDPressureUnitConverter.h"
-#import "DDRadiationUnitConverter.h"
-#import "DDTemperatureUnitConverter.h"
-#import "DDTemperatureUnitConverter.h"
-#import "DDTimeUnitConverter.h"
-#import "DDTorqueUnitConverter.h"
-#import "DDVelocityUnitConverter.h"
-#import "DDVolumeUnitConverter.h"
-
-//All converters imports end here.
-
 @interface DDUnitConverter : NSObject
 
 - (NSNumber *)convertNumber:(NSNumber *)number fromUnit:(DDUnit)fromUnit toUnit:(DDUnit)toUnit;
